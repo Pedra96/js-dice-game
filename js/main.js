28c16994d71d57f23d27254cc0e4d5f4f62fd23d
Pedra96 <darkraidialga@yahoo.it>
2022-11-13 21:58:56 +0100

    bugfixes

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@ let roll = document.getElementById("inputState").value;
 
 function rollDice() {
     console.log(roll);
-    let risultatoUser = "";
-    let risultatoComputer = "";
+    let risultatoUser = 0;
+    let risultatoComputer = 0;
     let NofRolls = parseInt(roll);
     console.log(NofRolls);
     for (let i = 0; i < NofRolls; i++) {
         console.log(roll);
-
         let randomNumber1 = Math.floor(Math.random() * 6) + 1;
         let randomNumber2 = Math.floor(Math.random() * 6) + 1;
         document.getElementById("diceGiocatore").innerHTML += '<img src="./img/face' + randomNumber1 + '.png" alt="dice logo" width="50" height="50"></img>';
@@ -18,22 +17,28 @@ function rollDice() {
     }
 
     if (risultatoUser === risultatoComputer) {
-        document.querySelector("h2").innerHTML = "Pareggio!";
+        document.querySelector("h2").innerHTML = "Pareggio!<br>"+risultatoUser+' = '+risultatoComputer;
     }
 
     else if (risultatoUser < risultatoComputer) {
         document.querySelector("h2").innerHTML
-            = ("HA VINTO IL COMPUTER!");
+            = "HA VINTO IL COMPUTER!<br>"+risultatoUser+' < '+risultatoComputer;
     }
 
     else {
         document.querySelector("h2").innerHTML
-            = ("HAI VINTO!");
+            = "HAI VINTO!<br>"+risultatoUser+' > '+risultatoComputer;
     }
     document.getElementById("bottoneprincipale").innerHTML = '<button class="btn btn-primary mt-2" type="submit" onClick="window.location.href=window.location.href">Vuoi riprovare?</button>';
 }
 
+//-------------------funzioni di controllo---------------------------------------
+document.getElementById("inputState").addEventListener("input",check);
 function check() {
     roll = document.getElementById("inputState").value;
+    if(roll<=0||roll>100){
+        document.getElementById("inputState").value=1;
+    }
     console.log(roll);
 }
+//-------------------------------------------------------------------------------
